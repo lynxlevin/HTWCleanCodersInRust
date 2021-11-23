0d7bb7f0957399248a70b332a713bdc5f4e727ea
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2021-11-23 14:48:10 +0900

    implement HuntTheWumpus and Command traits

--- htw/src/main.rs
+++ htw/src/main.rs
@@ -14,13 +14,13 @@ trait HtwMessageReceiver {
     fn hear_bats();
     fn hear_pit();
     fn smell_wumpus();
-    // fn passage(direction: HuntTheWumpus::Direction);
+    fn passage(direction: Direction);
     fn no_arrows();
     fn arrow_shot();
     fn player_shoots_self_in_back();
     fn player_kills_wumpus();
     fn player_shoots_wall();
-    fn arrows_found(arrows_found: i32);
+    fn arrows_found(arrows_found: u32);
     fn fell_in_pit();
     fn player_moves_to_wumpus();
     fn wumpus_moves_to_player();
@@ -43,5 +43,38 @@ impl Direction {
             Direction::East => Direction::West,
             Direction::West => Direction::East,
         }
+    }
+}
+
+trait HuntTheWumpus {
+    fn set_player_cavern(player_cavern: &str);
+
+    // fn getPlayerCavern() -> &str;
+    fn add_bat_cavern(cavern: &str);
+    fn add_pit_cavern(cavern: &str);
+    fn set_wumpus_cavern(wumpus_cavern: &str);
+    // fn getWumpusCavern() -> &str;
+    fn set_quiver(arrows: u32);
+    fn get_quiver() -> u32;
+    fn get_arrows_in_cavern(cavern: &str) -> u32;
+    fn connect_cavern(from: &str, to: &str, direction: Direction);
+    fn find_destination(cavern: &str, direction: Direction) -> &str;
+    // HuntTheWumpusGame.Command makeRestCommand(); // original code; a mistake? it should return HuntTheWumpus.Command
+    fn make_rest_command() -> Box<dyn Command>;
+    // HuntTheWumpusGame.Command makeShootCommand(Direction direction); // original code; a mistake? it should return HuntTheWumpus.Command
+    fn make_shoot_command(direction: Direction) -> Box<dyn Command>;
+    // HuntTheWumpusGame.Command makeMoveCommand(Direction direction); // original code; a mistake? it should return HuntTheWumpus.Command
+    fn make_move_command(direction: Direction) -> Box<dyn Command>;
+}
+
+trait Command {
+    fn execute(&self);
+}
+
+// just to kill error
+struct Test {}
+impl Command for Test {
+    fn execute(&self) {
+        println!("test");
     }
 }
