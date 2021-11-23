a2f43d9cf3fdea84da81c626419f169f0143815a
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2021-11-23 09:42:23 +0900

    trait HtwMessageReceiver
    imple in main
    will change to another file later, when dependencies are clear

--- HTW/src/main.rs
+++ HTW/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
 }
+
+trait HtwMessageReceiver {
+    fn no_passage();
+    fn hear_bats();
+    fn hear_pit();
+    fn smell_wumpus();
+    // fn passage(direction: HuntTheWumpus::Direction);
+    fn no_arrows();
+    fn arrow_shot();
+    fn player_shoots_self_in_back();
+    fn player_kills_wumpus();
+    fn player_shoots_wall();
+    fn arrows_found(arrows_found: i32);
+    fn fell_in_pit();
+    fn player_moves_to_wumpus();
+    fn wumpus_moves_to_player();
+    fn bats_transport();
+}
